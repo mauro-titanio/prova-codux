5960342a2ee98018c29d41c60549f1a77eb75ee2
Mauro Titanio <maurotitanio@gmail.com>
2023-12-18 17:18:15 +0100

    update

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -4,4 +4,9 @@ import App from '../../../App';
 export default createBoard({
     name: 'App',
     Board: () => <App />,
+    environmentProps: {
+        canvasWidth: 1560,
+        windowWidth: 1002,
+        canvasHeight: 655
+    }
 });
---
+++ src/components/button/button.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import Button, { ButtonProps } from '@mui/material/Button';
+export interface CustomButtonProps extends ButtonProps { }
+export function CustomButton(props: CustomButtonProps) {
+    console.log(props);
+    return <Button {...props} color="secondary" variant="contained" />;
+}
